aa6a2b3466bb0948684e729a75eed06886f48755
longhd1703 <hoanglonggg356@gmail.com>
2024-05-28 01:04:48 +0700

    udate

--- chat-V2/FE/src/components/StyleInput.js
+++
@@ -1,9 +0,0 @@
-import { styled, TextField } from "@mui/material";
-const StyleInput = styled(TextField)(({ theme }) => ({
-  "& .MuiInputBase-input": {
-    paddingTop: "12px",
-    paddingBottom: "10px",
-    
-  }
-}));
-export default StyleInput;
