0426792a03e876a8e13682167959467cf2b6b746
longhd <longhd@fintwin.vn>
2024-09-09 10:47:00 +0700

    add verification otp account

---
+++ Node-js-Auth/models/otp.js
@@ -0,0 +1,100 @@
+const otpGenerator = require("otp-generator"); 
+
+const mongoose = require("mongoose");
+const mailSender = require("../utils/mailSender");
+
+const OTPSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  otp: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: 60 * 5, // The document will be automatically deleted after 5 minutes of its creation time
+  },
+});
+
+// Define a function to send emails
+async function sendVerificationEmail(email, otp) {
+  // Send the email using our custom mailSender Function
+  try {
+    const mailResponse = await mailSender(
+      email,
+      "Verification Email",
+      `<h1>Please confirm your OTP </h1>
+             <p> here is your OTP code:-> ${otp} </p>
+            `
+    );
+    console.log("Email sent successfully: ", mailResponse);
+  } catch (error) {
+    console.log("Error occurred while sending email: ", error);
+    throw error;
+  }
+}
+
+OTPSchema.pre("save", async function (next) {
+  console.log("New document saved to database");
+
+  // Only send an email when a new document is created
+  if (this.isNew) {
+    await sendVerificationEmail(this.email, this.otp);
+  }
+  next();
+});
+
+const OTP = mongoose.model("OTP", OTPSchema);
+
+module.exports = OTP;
+
+
+// Send OTP For Email Verification
+exports.sendotp = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    // Check if user is already present
+    // Find user with provided email
+    const checkUserPresent = await user.findOne({ email });
+    // to be used in case of signup
+
+    // If user found with provided email
+    if (checkUserPresent) {
+      // Return 401 Unauthorized status code with error message
+      return res.status(401).json({
+        success: false,
+        message: `User is Already Registered`,
+      });
+    }
+
+    var otp = otpGenerator.generate(6, {
+      upperCaseAlphabets: false,
+      lowerCaseAlphabets: false,
+      specialChars: false,
+    });
+    const result = await OTP.findOne({ otp: otp });
+    console.log("Result is Generate OTP Func");
+    console.log("OTP", otp);
+    console.log("Result", result);
+    while (result) {
+      otp = otpGenerator.generate(6, {
+        upperCaseAlphabets: false,
+      });
+    }
+    const otpPayload = { email, otp };
+    const otpBody = await OTP.create(otpPayload);
+    console.log("OTP Body", otpBody);
+    res.status(200).json({
+      success: true,
+      message: `OTP Sent Successfully`,
+      otp,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ success: false, error: error.message });
+  }
+};
